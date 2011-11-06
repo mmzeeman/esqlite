ca68887b179cecec40b1f40dd1eed2bac467ba8c
Maas-Maarten Zeeman <maas@channel.me>
2011-11-06 21:57:59 +0100

    Added copyright notice

--- src/esqlite3_nif.erl
+++ src/esqlite3_nif.erl
@@ -1,6 +1,21 @@
+%% @author Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
+%% @copyright 2011 Maas-Maarten Zeeman
+
+%% @doc Low level erlang API for sqlite3 databases
+
+%% Copyright 2011 Maas-Maarten Zeeman
 %%
-%%
-%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%% 
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%% 
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
 
 -module(esqlite3_nif).
 -author("Maas-Maarten Zeeman <mmzeeman@xs4all.nl>").
